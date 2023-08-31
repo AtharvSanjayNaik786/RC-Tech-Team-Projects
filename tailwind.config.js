b31988b38b4119ddd5d3c354c709bdd3b7f5a536
AtharvNaik <atharvsanjay.naik.phy21@itbhu.ac.in>
2023-08-31 18:57:07 +0530

    Task 1 Completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      space:{
+        '50px':'50px'
+      }
+    },
+  },
+  plugins: [],
+}
+
